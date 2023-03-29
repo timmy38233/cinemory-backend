ed77f7fa108e8f833a2c3ff10c8cbb66cad5e120
Tim <github@mailtim.de>
2023-03-29 13:10:59 +0200

    Add database connection

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -1,12 +1,24 @@
 <?php
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HealthCheckController extends AbstractController
 {
-    public function getHealthCheck()
+    public function __construct(
+        public EntityManagerInterface $entityManager
+    ) {
+    }
+    public function getHttpHealthCheck()
     {
-        return $this->json(['status' => "I'm alive"]);
+        return $this->json(['status' => 'reachable']);
+    }
+
+    public function getDatabaseHealthCheck()
+    {
+        return $this->json(
+            ['status' => $this->entityManager->getConnection()->connect() ? 'connected' : 'dead']
+        );
     }
 }
